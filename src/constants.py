54da582c243763a12667f0c13957c418fd1cbdbd
BetulioMay <cesarmay@protonmail.com>
2022-12-14 17:05:48 +0100

    Editado menu principal (No implementado)

--- src/constants.py
+++ src/constants.py
@@ -1,31 +1,34 @@
-'''
+"""
 Constantes o variables globales.
-'''
+"""
 
 import os
 from enum import Enum
 
 # Credenciales
-HOST=os.getenv("ORACLE_DB_HOST")
-PORT=os.getenv("ORACLE_DB_PORT")
-USER=os.getenv("ORACLE_DB_USER")
-PASSWD=os.getenv("ORACLE_DB_PASSWD")
-DSN=os.getenv("ORACLE_DB_DNS")
+HOST = os.getenv("ORACLE_DB_HOST")
+PORT = os.getenv("ORACLE_DB_PORT")
+USER = os.getenv("ORACLE_DB_USER")
+PASSWD = os.getenv("ORACLE_DB_PASSWD")
+DSN = os.getenv("ORACLE_DB_DNS")
 
 # Enums
 class MENU_OPTION(Enum):
-	CREATE_TABLE = 1
-	DROP_TABLE = 2
-	REGISTER_ORDER = 3
-	SHOW_TABLES = 4
-	EXIT_MENU = 5
+    CREATE_TABLES = 1
+    PAREJAS_ENTRENADOR = 2
+    PAGAR_COMPRA = 3
+    GUARDAR_PARTIDO = 4
+    DROP_TABLES = 5
+    EXIT_MENU = 6
+
 
 class ORDER_OPTION(Enum):
-	ADD_DETAIL = 1
-	DELETE_DETAILS = 2
-	CANCEL_ORDER = 3
-	FINISH_ORDER = 4
+    ADD_DETAIL = 1
+    DELETE_DETAILS = 2
+    CANCEL_ORDER = 3
+    FINISH_ORDER = 4
+
 
 # Styles
-HEADER = "-"*15
+HEADER = "-" * 15
 IN_PROMPT = "> "
--- src/run.py
+++ src/run.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
-'''
+"""
 Programa principal.
 Aqui se ejecutara el `main loop` del programa
-'''
+"""
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 import connection
@@ -15,37 +16,46 @@ import transactions.register_order as register_order
 from transactions.drop_insert import drop_tables, create_tables, insert_stock
 from transactions.show_tables import show_tables
 
-def run():
-	conn = connection.connect()
-	cursor = connection.get_cursor(conn)
 
-	finish = False
-	option = None
-	while not finish:
-		option = main.main_menu()
-		match option:
-			case MENU_OPTION.CREATE_TABLE.value:
-				if create_tables(cursor=cursor) and insert_stock(cursor=cursor):
-					print("Stock creado.")
-					connection.commit(conn)
-				else:
-					print("No se han conseguido crear el stock.")
-			case MENU_OPTION.DROP_TABLE.value:
-				if drop_tables(cursor=cursor):
-					print("Tablas borradas")
-					connection.commit(conn)
-				else:
-					print("No se han conseguido borrar las tablas")				
-			case MENU_OPTION.REGISTER_ORDER.value:
-				register_order.register_order(conn=conn, cursor=cursor)
-			case MENU_OPTION.SHOW_TABLES.value:
-				show_tables(cursor=cursor)
-			case MENU_OPTION.EXIT_MENU.value:
-				connection.close_cursor(cursor=cursor)
-				connection.close_connection(conn=conn)
-				print("BYE.")
-				finish = True
+def run():
+    conn = connection.connect()
+    cursor = connection.get_cursor(conn)
+
+    finish = False
+    option = None
+    while not finish:
+        option = main.main_menu()
+        match option:
+            case MENU_OPTION.CREATE_TABLES.value:
+                # TODO: Crear otro insert `insert_tables` donde se inserten los datos
+                # que se necesiten, en vez de insert_stock (codigo viejo)
+                if create_tables(cursor=cursor) and insert_stock(cursor=cursor):
+                    print("Tablas creadas.")
+                    connection.commit(conn)
+                else:
+                    print("No se han conseguido crear el stock.")
+            case MENU_OPTION.DROP_TABLES.value:
+                if drop_tables(cursor=cursor):
+                    print("Tablas borradas")
+                    connection.commit(conn)
+                else:
+                    print("No se han conseguido borrar las tablas")
+            case MENU_OPTION.PAREJAS_ENTRENADOR.value:
+                # TODO: Implementar funcion que muestre las tablas de las parejas
+                # que un entrenador entrena en una edicion (RF1.4)
+                print("Not implemented.")
+            case MENU_OPTION.PAGAR_COMPRA.value:
+                # TODO: Implementar funcion de insertar una nueva compra pagada (RF2.4)
+                print("Not implemented.")
+            case MENU_OPTION.GUARDAR_PARTIDO.value:
+                # TODO: Implementar funcion de insertar un nuevo partido RF(3.1)
+                print("Not implemented.")
+            case MENU_OPTION.EXIT_MENU.value:
+                connection.close_cursor(cursor=cursor)
+                connection.close_connection(conn=conn)
+                print("BYE.")
+                finish = True
+
 
 if __name__ == "__main__":
-	run()
-
+    run()
