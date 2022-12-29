e4f8254ed751f916101b741464a4a1faa1758c9c
adrianldga <adrianldga@correo.ugr.es>
2022-12-29 12:57:53 +0100

    Update Tablas_Adrian.sql

--- db/Tablas_Adrian.sql
+++ db/Tablas_Adrian.sql
@@ -61,3 +61,22 @@ create table hecha(
     cod_edicion references edicion(cod_edicion) not null,
     primary key (cod_compra)
 );
+
+-- Disparador RS2.1
+--Un espectador solo puede pagar una compra por día.Si el espectador ya había realizado una compra y la había pagado, no se le dejará pagar otra compra y se devuelve un mensaje de error”.
+ insert into espectador(dni,nombre) values ('7557761','Pepe');
+ insert into espectador(dni,nombre) values ('577618','Paco');
+ insert into comprador values ('577618');
+ insert into comprador values ('7557761');
+ insert into compra values ('1235448');
+ insert into compra values ('321');
+
+ --inserta bien
+ insert into paga values ('1235448', '28/11/2002-20:04:30', '577618');
+ insert into paga values ('321', '30/11/2002-20:04:30', '7557761');
+ --da error
+ insert into paga values ('321', '30/11/2002-23:04:30', '7557761');
+ --da error
+ insert into paga values ('321', '31/11/2002-17:04:30', '7557761');
+ --inserta bien
+ insert into paga values ('321', '5/12/2002-23:04:30', '7557761');
