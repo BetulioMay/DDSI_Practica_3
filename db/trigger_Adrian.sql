a5acaa021064c149d25a908ddf2186a469e113da
adrianldga <adrianldga@correo.ugr.es>
2023-01-01 16:05:14 +0100

    Versión_Definitiva_trigger_Adrian.sql

--- db/trigger_Adrian.sql
+++ db/trigger_Adrian.sql
@@ -3,15 +3,12 @@ before insert on paga
 for each row
 declare
   cursor pago_comprador is select * from paga p where :new.dni = p.dni;
-  fecha timestamp;
   dif_fechas float;
 begin
-  select fecha_pago into fecha from paga p where p.dni = :new.dni;
-
   for regPago in pago_comprador loop
     dif_fechas := abs(cast(to_timestamp(:new.fecha_pago) as  date) - cast(to_timestamp(regPago.fecha_pago) as date));
-    if (dif_fechas >= 1) then
-      raise_application_error(-20600, :new.dni || 'No se puede realizar otro pago');
+    if (dif_fechas < 1) then
+      raise_application_error(-20600, :new.dni || ' No se puede realizar otro pago');
     end if;
   end loop;
 end;
